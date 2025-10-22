9c3123ed74c6be02249cd6db55a72c6023a7fcbb
Ch4120N <charonsecagency@gmail.com>
2025-10-22 10:20:23 +0330

    Added required library & ImportError handling for required libarries

--- ChAsciiGen.py
+++ ChAsciiGen.py
@@ -1,3 +1,18 @@
 # -*- coding: UTF-8 -*-
 # ChAsciiGen.py
 
+import sys
+
+try:
+    import pyfiglet
+    from colorama import Fore, init
+    init(autoreset=True)
+except ImportError:
+    sys.exit(
+        '[ - ] Please install the required libraries. Use the following commands to install:\n'
+        '\t - python -m pip install colorama pyfiglet pyreadline3\n'
+        '\t OR\n'
+        '\t - python -m pip install -r requirements.txt\n\n'
+        'If you are using the Linux operating system, you must install the required libraries globally using the package manager\n'
+        'Or create a virtual environment and then install the required libraries.\n'
+    )
