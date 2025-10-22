5a68ab35f9bb7e609ef9ebf012e28db623c4c756
Ch4120N <charonsecagency@gmail.com>
2025-10-22 15:20:08 +0330

    Created Interactive class base on cmd lib

--- cli/interactive.py
+++ cli/interactive.py
@@ -1,3 +1,22 @@
 # -*- coding: UTF-8 -*-
-# cli/parser.py
+# cli/interactive.py
 
+from __future__ import annotations
+
+import cmd
+import sys
+import signal
+
+
+
+
+class Interactive(cmd.Cmd):
+    prompt = 'root@ChAsciiGen :~# '
+    def __init__(self, completekey: str = "tab", stdin = None, stdout = None) -> None:
+        super().__init__(completekey, stdin, stdout)
+    
+    def emptyline(self) -> bool:
+        return True
+    
+    def do_list_fonts(self, args):
+        print('Fonts: standard')
