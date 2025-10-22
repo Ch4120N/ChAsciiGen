47eb2eb8d7eb9ba705178d362d3a824e9c6dced4
Ch4120N <charonsecagency@gmail.com>
2025-10-22 17:05:26 +0330

    Added command not found var

--- core/config.py
+++ core/config.py
@@ -4,6 +4,8 @@
 from colorama import Fore, init
 init(autoreset=True)
 
+from ui.colorize import colorize
+
 
 __version__ = '1.0'
 
@@ -11,14 +13,15 @@ SCRIPT_NAME = 'ChAsciiGen'
 
 SCRIPT_DESCRIPTION = 'Simple tool for text to ASCII Art generator'
 
-PROMPT = (
-    Fore.LIGHTRED_EX + ' root' +
-    Fore.LIGHTWHITE_EX + '@' +
-    Fore.LIGHTBLUE_EX + 'ChAsciiGen' +
-    Fore.LIGHTWHITE_EX + ' :' +
-    Fore.LIGHTBLUE_EX + '~' +
-    Fore.LIGHTRED_EX + '# '
-)
+PROMPT = str(colorize(
+    '%BoldRed', 'root',
+    '%BoldWhite', '@',
+    '%BoldBlue', 'ChAsciiGen',
+    '%BoldRed', ' ~# '
+))
+
+COMMAND_NOT_FOUND = f"{Fore.LIGHTRED_EX}{SCRIPT_NAME} {Fore.LIGHTWHITE_EX}: command {Fore.LIGHTRED_EX}'%s'{Fore.LIGHTWHITE_EX} not found.\n"
+
 class Config:
     MAX_WIDTH: int = 80
     DEFAULT_FONT: str = 'standard'
