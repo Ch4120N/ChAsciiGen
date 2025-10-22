d00b787591d84dacb6070611ad8685839f47defb
Ch4120N <charonsecagency@gmail.com>
2025-10-22 17:27:59 +0330

    Change MainBanner function to string return

--- ui/banner.py
+++ ui/banner.py
@@ -4,17 +4,17 @@
 from colorama import Fore, init
 init(autoreset=True)
 
-def MainBanner():
-    print(fr"{Fore.LIGHTRED_EX}   ___ _       _            _ _   ___           ")
-    print(fr"{Fore.LIGHTRED_EX}  / __\ |__   /_\  ___  ___(_|_) / _ \___ _ __  ")
-    print(fr"{Fore.LIGHTRED_EX} / /  | '_ \ //_\\/ __|/ __| | |/ /_\/ _ \ '_ \ ")
-    print(fr"{Fore.LIGHTRED_EX}/ /___| | | /  _  \__ \ (__| | / /_\\  __/ | | |")
-    print(fr"{Fore.LIGHTRED_EX}\____/|_| |_\_/ \_/___/\___|_|_\____/\___|_| |_|")
-    print()
-    print(f' {Fore.YELLOW}╔═════════════════════════════════════════════╗')
-    print(f' {Fore.YELLOW}║                 {Fore.LIGHTCYAN_EX}ChAsciiGen                  {Fore.YELLOW}║')
-    print(f' {Fore.YELLOW}║{Fore.LIGHTWHITE_EX} Simple tool for text to ASCII Art generator {Fore.YELLOW}║')
-    print(f' {Fore.YELLOW}║ {Fore.LIGHTGREEN_EX}Author  {Fore.LIGHTWHITE_EX}:  {Fore.LIGHTRED_EX}Ch4120N                          {Fore.YELLOW}║')
-    print(f' {Fore.YELLOW}║ {Fore.LIGHTGREEN_EX}Github  {Fore.LIGHTWHITE_EX}:  {Fore.LIGHTRED_EX}Github.com/Ch4120N               {Fore.YELLOW}║')
-    print(f' {Fore.YELLOW}╚═════════════════════════════════════════════╝')
-    print()
+def MainBanner() -> str:
+    return (
+        f"{Fore.LIGHTRED_EX}    ___ _       _            _ _   ___           "
+        f"{Fore.LIGHTRED_EX}   / __\\ |__   /_\\  ___  ___(_|_) / _ \\___ _ __  "
+        f"{Fore.LIGHTRED_EX}  / /  | '_ \\ //_\\/ __|/ __| | |/ /_\\/ _ \\ '_ \\ "
+        f"{Fore.LIGHTRED_EX} / /___| | | /  _  \\__ \\ (__| | / /_\\  __/ | | |"
+        f"{Fore.LIGHTRED_EX} \\____/|_| |_\\_/ \\_/___/\\___|_|_\\____/\\___|_| |_|"
+        f' {Fore.YELLOW}╔═════════════════════════════════════════════╗'
+        f' {Fore.YELLOW}║                 {Fore.LIGHTCYAN_EX}ChAsciiGen                  {Fore.YELLOW}║'
+        f' {Fore.YELLOW}║{Fore.LIGHTWHITE_EX} Simple tool for text to ASCII Art generator {Fore.YELLOW}║'
+        f' {Fore.YELLOW}║ {Fore.LIGHTGREEN_EX}Author  {Fore.LIGHTWHITE_EX}:  {Fore.LIGHTRED_EX}Ch4120N                          {Fore.YELLOW}║'
+        f' {Fore.YELLOW}║ {Fore.LIGHTGREEN_EX}Github  {Fore.LIGHTWHITE_EX}:  {Fore.LIGHTRED_EX}Github.com/Ch4120N               {Fore.YELLOW}║'
+        f' {Fore.YELLOW}╚═════════════════════════════════════════════╝'
+    )
