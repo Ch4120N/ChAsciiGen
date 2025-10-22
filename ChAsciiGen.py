7a06986a8b0b5a42636f901ab63400edbc7c24d7
Ch4120N <charonsecagency@gmail.com>
2025-10-22 14:00:06 +0330

    Added ChAsciiGen modules and created main class

--- ChAsciiGen.py
+++ ChAsciiGen.py
@@ -9,10 +9,26 @@ try:
     init(autoreset=True)
 except ImportError:
     sys.exit(
-        '[ - ] Please install the required libraries. Use the following commands to install:\n'
-        '\t - python -m pip install colorama pyfiglet pyreadline3\n'
-        '\t OR\n'
-        '\t - python -m pip install -r requirements.txt\n\n'
+        '[ - ] Please install the required libraries. Use the following commands to install:\n\n'
+        '\t- python -m pip install colorama pyfiglet pyreadline3\n'
+        '\tOR\n'
+        '\t- python -m pip install -r requirements.txt\n\n'
         'If you are using the Linux operating system, you must install the required libraries globally using the package manager\n'
         'Or create a virtual environment and then install the required libraries.\n'
     )
+
+from ui.banner import MainBanner
+from ui.decorators import MsgDCR
+
+
+class ChAsciiGen:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = ChAsciiGen()
+    sys.exit(app.run())
