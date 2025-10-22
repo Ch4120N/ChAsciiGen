5f4f9f83ea788331b9f4bac109d62adc2a31545f
Ch4120N <charonsecagency@gmail.com>
2025-10-22 17:36:49 +0330

    Updated banner.py

--- ui/banner.py
+++ ui/banner.py
@@ -6,15 +6,20 @@ init(autoreset=True)
 
 def MainBanner() -> str:
     return (
-        f"{Fore.LIGHTRED_EX}    ___ _       _            _ _   ___           "
-        f"{Fore.LIGHTRED_EX}   / __\\ |__   /_\\  ___  ___(_|_) / _ \\___ _ __  "
-        f"{Fore.LIGHTRED_EX}  / /  | '_ \\ //_\\/ __|/ __| | |/ /_\\/ _ \\ '_ \\ "
-        f"{Fore.LIGHTRED_EX} / /___| | | /  _  \\__ \\ (__| | / /_\\  __/ | | |"
-        f"{Fore.LIGHTRED_EX} \\____/|_| |_\\_/ \\_/___/\\___|_|_\\____/\\___|_| |_|"
-        f' {Fore.YELLOW}╔═════════════════════════════════════════════╗'
-        f' {Fore.YELLOW}║                 {Fore.LIGHTCYAN_EX}ChAsciiGen                  {Fore.YELLOW}║'
-        f' {Fore.YELLOW}║{Fore.LIGHTWHITE_EX} Simple tool for text to ASCII Art generator {Fore.YELLOW}║'
-        f' {Fore.YELLOW}║ {Fore.LIGHTGREEN_EX}Author  {Fore.LIGHTWHITE_EX}:  {Fore.LIGHTRED_EX}Ch4120N                          {Fore.YELLOW}║'
-        f' {Fore.YELLOW}║ {Fore.LIGHTGREEN_EX}Github  {Fore.LIGHTWHITE_EX}:  {Fore.LIGHTRED_EX}Github.com/Ch4120N               {Fore.YELLOW}║'
-        f' {Fore.YELLOW}╚═════════════════════════════════════════════╝'
+        f"{Fore.LIGHTRED_EX}    ___ _       _            _ _   ___           \n"
+        f"{Fore.LIGHTRED_EX}    / __\\ |__   /_\\  ___  ___(_|_) / _ \\___ _ __  \n"
+        f"{Fore.LIGHTRED_EX}  / /  | '_ \\ //_\\\\/ __|/ __| | |/ /_\\/ _ \\ '_ \\ \n"
+        f"{Fore.LIGHTRED_EX} / /___| | | /  _  \\__ \\ (__| | / /_\\  __/ | | |\n"
+        f"{Fore.LIGHTRED_EX} \\____/|_| |_\\_/ \\_/___/\\___|_|_\\____/\\___|_| |_|\n"
+        '\n'
+    )
+
+def MainSubBanner() -> str:
+    return (
+        f'{Fore.YELLOW}╔═════════════════════════════════════════════╗\n'
+        f'{Fore.YELLOW}║                 {Fore.LIGHTCYAN_EX}ChAsciiGen                  {Fore.YELLOW}║\n'
+        f'{Fore.YELLOW}║{Fore.LIGHTWHITE_EX} Simple tool for text to ASCII Art generator {Fore.YELLOW}║\n'
+        f'{Fore.YELLOW}║ {Fore.LIGHTGREEN_EX}Author  {Fore.LIGHTWHITE_EX}:  {Fore.LIGHTRED_EX}Ch4120N                          {Fore.YELLOW}║\n'
+        f'{Fore.YELLOW}║ {Fore.LIGHTGREEN_EX}Github  {Fore.LIGHTWHITE_EX}:  {Fore.LIGHTRED_EX}Github.com/Ch4120N               {Fore.YELLOW}║\n'
+        f'{Fore.YELLOW}╚═════════════════════════════════════════════╝\n'
     )
